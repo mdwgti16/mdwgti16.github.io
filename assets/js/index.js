d1038f73faf2a623559bfe0a27c4b064d4ed8928
mdwgti16 <mdwgti16@gmail.com>
2020-01-06 16:41:28 +0900

    edit the title of the env'

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,10 +6,19 @@ $('.page__content > p > img[alt]').replaceWith(function () {
         + '<figcaption class="caption">' + $(this).attr('alt') + '</figcaption>'
         + '</figure>';
   });
-  
+
+$('.page__content > ul > li img[alt]').replaceWith(function () {
+  return '<figure>'
+      + '<a href="' + $(this).attr('src') + '" class="mg-link">'
+      + '<img src="' + $(this).attr('src') + '"/></a>'
+      + '<figcaption class="caption">' + $(this).attr('alt') + '</figcaption>'
+      + '</figure>';
+});  
 // 이미지를 magnific popup image viewer에 연결시킴
 $('.mg-link').magnificPopup({
 type: 'image',
 closeOnContentClick: true
 });
-  
+
+// $(".page__content > ul > li img[src]").replaceWith("a")
+// $(".page__content > p img[src]").replaceWith("a")
